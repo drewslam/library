517246b2edbfd7b4da9d4374f7d89b6344fee968
Andrew Souza <andrewesouza@gmail.com>
2021-10-26 22:40:53 -0700

    "first commit"

---
+++ app.js
@@ -0,0 +1,16 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    /* this.info = function() {
+        return `${title} by ${author}, ${pages} pages, ${read}`
+    } */
+}
+
+function addBookToLibrary() {
+    const newBook = new Book(title, author, pages, read);
+    return myLibrary.push(newBook);
+}
