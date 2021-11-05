0f1cc5d7ff881c55b9594d0f650eb1a2fca96954
Andrew Souza <andrewesouza@gmail.com>
2021-11-04 23:19:02 -0700

    update

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const title = document.querySelector('.title');
+const author = document.querySelector('.author');
+const pages = document.querySelector('.pages');
+const checkBox = document.querySelector('.read');
+const submit = document.querySelector('.submit');
+const content = document.querySelector('.content');
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -11,6 +18,12 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-    const newBook = new Book(title, author, pages, read);
+    const newBook = new Book(title.value, author.value, pages.value, checkBox.checked);
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+    checkBox.checked = false;
     return myLibrary.push(newBook);
 }
+
+submit.addEventListener('click', addBookToLibrary)
