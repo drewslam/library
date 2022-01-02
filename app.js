3c1bae3ab67ca4c887bcbbc04977b19a73d0e284
drewslam <andrewesouza@gmail.com>
2022-01-02 10:56:06 -0800

    updated display function

--- app.js
+++ app.js
@@ -9,21 +9,29 @@ const submit = document.querySelector('.submit');
 const content = document.querySelector('.content');
 
 // initialize library variable
-let myLibrary = [{ title: 'the Iliad', author: 'Homer', pages: 297, read: false }, { title: 'White Noise', author: 'Don DeLilo', pages: 326, read: true }];
+let myLibrary = [
+    // dummy books
+    {
+        title: 'the Iliad',
+        author: 'Homer',
+        pages: 297,
+        read: false
+    },
+    {
+        title: 'White Noise',
+        author: 'Don DeLilo',
+        pages: 326,
+        read: true
+    }
+];
+let contentLib = [...content.children];
 
 // Book constructor
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
-    this.pages = parseInt(pages);
+    this.pages = pages;
     this.read = read;
-    /* this.readStatus = function() {
-        if (read === true) {
-            return 'Read';
-        } else {
-            return 'Unread';
-        }
-    } */
 }
 
 // add book to array
@@ -38,24 +46,32 @@ function addBookToLibrary() {
     author.value = '';
     pages.value = '';
     checkBox.checked = false;
+    displayLibrary();
 }
 
 // display library on page
 function displayLibrary() {
     myLibrary.forEach(book => {
         const div = document.createElement('div');
+        div.setAttribute('data-number', myLibrary.indexOf(book));
         const btn = document.createElement('button');
-        const p1 = document.createElement('p');
-        const p2 = document.createElement('p');
-        const p3 = document.createElement('p');
-        p1.textContent = book.title;
-        p2.textContent = `by ${book.author}`;
-        p3.textContent = `${book.pages} pages`;
+        btn.classList.add('btn');
+        const deleteBtn = document.createElement('button');
+        deleteBtn.classList.add('delete');
+        deleteBtn.textContent = 'Delete';
         content.appendChild(div).classList.add('book');
-        div.appendChild(p1);
-        div.appendChild(p2);
-        div.appendChild(p3);
-        div.appendChild(btn).classList.add('btn');
+        div.innerHTML = `
+                        <p class='title'>${book.title}</p>
+                        <p class='author'>by ${book.author}</p>
+                        <p class='pages'>${book.pages} pages</p>
+                        `;
+        div.appendChild(btn);
+        div.appendChild(deleteBtn);
+
+        if (div.getAttribute('data-number')) {
+            contentLib.push(div)
+        }
+
         if (book.read === true) {
             btn.classList.add('read');
             btn.textContent = 'Read';
@@ -63,30 +79,44 @@ function displayLibrary() {
             btn.classList.add('unread');
             btn.textContent = 'Unread';
         }
-    })
-}
 
-displayLibrary();
 
-// initialize read/unread buttons
-const readUnread = document.querySelectorAll('.btn');
+        btn.addEventListener('click', () => {
+            if (btn.classList.contains('unread')) {
+                book.read = true;
+                btn.classList.remove('unread');
+                btn.classList.add('read');
+                btn.textContent = 'Read';
+            } else {
+                book.read = false;
+                btn.classList.remove('read');
+                btn.classList.add('unread');
+                btn.textContent = 'Unread';
+            }
+        })
 
-// read/unread button toggle
-readUnread.forEach(btn => {
-    btn.addEventListener('click', () => {
-        if (btn.classList.contains('unread')) {
-            btn.classList.remove('unread');
-            btn.classList.add('read');
-            btn.textContent = 'Read';
-        } else {
-            btn.classList.remove('read');
-            btn.classList.add('unread');
-            btn.textContent = 'Unread';
+        deleteBtn.addEventListener('click', () => {
+            myLibrary.splice(book, 1);
+            content.removeChild(div);
+        })
+
+        // prevent duplicate listings
+        for (let i = 0; i < contentLib.length; i++) {
+            let counter = 0;
+            for (let j = 0; j < contentLib.length; j++) {
+                if (contentLib[i].getAttribute('data-number') === contentLib[j].getAttribute('data-number')) {
+                    counter++
+                }
+            }
+            if (counter > 1) {
+                content.removeChild(contentLib[(i)]);
+                contentLib.splice((i), 1);
+            }
         }
     })
-})
-
+}
 
 // submit button on form
 submit.addEventListener('click', addBookToLibrary)
 
+displayLibrary();
